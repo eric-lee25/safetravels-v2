db70aec5a23fe146d294b715240136f250102de1
tabvn.drupal <tabvn.drupal@3378938.no-reply.drupal.org>
2017-01-08 08:40:01 +0700

    Toan

--- src/app/product-offers/product-offers.component.ts
+++ src/app/product-offers/product-offers.component.ts
@@ -1,27 +1,44 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import {DxChartModule, DxSelectBoxModule} from 'devextreme-angular';
 import {OfferService, OfferInfo} from "../services/offer.service";
 
 
 @Component({
-  selector: 'app-product-offers',
-  templateUrl: './product-offers.component.html',
-  styleUrls: ['./product-offers.component.css'],
-  providers: [OfferService]
+	selector: 'app-product-offers',
+	templateUrl: './product-offers.component.html',
+	styleUrls: ['./product-offers.component.css'],
+	providers: [OfferService]
 })
 export class ProductOffersComponent implements OnInit {
 
 
-  OffersInfo: OfferInfo[];
+	OffersInfo: OfferInfo[];
 
-  constructor(private offerService: OfferService) {
+	@ViewChild('childrenOfferTable') childrenOfferTable: ElementRef;
 
-    this.OffersInfo = this.offerService.getOffersInfo();
+	tableCollapsed: boolean[] = [];
 
-  }
 
-  ngOnInit() {
+	constructor(private offerService: OfferService) {
 
-  }
+		this.OffersInfo = this.offerService.getOffersInfo();
+
+	}
+
+	ngOnInit() {
+
+	}
+
+
+	showChildrenOfferTable(id: number) {
+
+		if (this.tableCollapsed[id] == null) {
+			this.tableCollapsed.push(true);
+		} else {
+			this.tableCollapsed[id] = !this.tableCollapsed[id];
+		}
+
+
+	}
 
 }
