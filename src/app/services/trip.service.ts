7d4b227e96e2f697245ed00a24f7d333c5bae7b3
Toan <toan@tabvn.com>
2017-01-20 09:53:58 +0700

    Trip list

--- src/app/services/trip.service.ts
+++ src/app/services/trip.service.ts
@@ -6,23 +6,45 @@ import {Trip} from "../models/trip.model";
 @Injectable()
 export class TripService {
 
-	constructor(private appService: AppService) {
+  constructor(private appService: AppService) {
 
-	}
+  }
 
 
-	listTrips(): Observable<Trip[]> {
-		return this.appService.get('/trips?role=user').map(res => res.json().data).catch(err => Observable.throw(err))
+  listTrips(include?: string): Observable<Trip[]> {
+    let endpoint = '/trips?role=user';
+    if (include) {
+      endpoint = '/trips?role=user&include=' + include;
+    }
+    return this.appService.get(endpoint).map(res => this.formatTrips(res.json().data)).catch(err => Observable.throw(err))
 
-	}
+  }
 
-	get(id: number, include?: string): Observable<Trip> {
+  get(id: number, include?: string): Observable<Trip> {
 
-		let endpoint = '/trips/' + id;
-		if (include) {
-			endpoint = '/trips/' + id + '?include=' + include;
-		}
-		return this.appService.get(endpoint).map(res => res.json().data).catch(err => Observable.throw(err));
-	}
+    let endpoint = '/trips/' + id;
+    if (include) {
+      endpoint = '/trips/' + id + '?include=' + include;
+    }
+    return this.appService.get(endpoint).map(res => res.json().data).catch(err => Observable.throw(err));
+  }
+
+  formatTrips(trips: Trip[]): Trip[] {
+
+
+    for (let x = 0; x < trips.length; x++) {
+
+      if (trips[x].admin && trips[x].admin.data) {
+        trips[x].admin = trips[x].admin.data;
+      }
+      if (trips[x].guide && trips[x].guide.data) {
+        trips[x].guide = trips[x].admin.data;
+      }
+
+    }
+
+    return trips;
+
+  }
 
 }
--- src/app/trips/trips/trips.component.ts
+++ src/app/trips/trips/trips.component.ts
@@ -1,23 +1,41 @@
 import {Component, OnInit} from '@angular/core';
 import {DialogService} from "../../services/dialog.service";
 import {AppService} from "../../services/app.service";
+import {Trip} from "../../models/trip.model";
+import {TripService} from "../../services/trip.service";
 
 @Component({
-	selector: 'app-trips',
-	templateUrl: './trips.component.html',
-	styleUrls: ['./trips.component.css']
+  selector: 'app-trips',
+  templateUrl: './trips.component.html',
+  styleUrls: ['./trips.component.css']
 })
 export class TripsComponent implements OnInit {
 
-	constructor(private dialogService: DialogService, private appService: AppService) {
-	}
+  trips: Trip[] = [];
+  showLoading: boolean = true;
 
-	ngOnInit() {
-	}
+  constructor(private tripService: TripService,
+              private dialogService: DialogService,
+              private appService: AppService) {
 
-	openAddTripDialog() {
-		this.appService.dialogTitleEvent.next("Create new Trip");
-		this.dialogService.openEditTripDialog();
-	}
+  }
+
+  ngOnInit() {
+
+    this.tripService.listTrips('admin,guide').subscribe(res => {
+      this.trips = res;
+      this.showLoading = false;
+
+    }, err => {
+      console.log(err);
+      this.showLoading = false;
+    });
+  }
+
+
+  openAddTripDialog() {
+    this.appService.dialogTitleEvent.next("Create new Trip");
+    this.dialogService.openEditTripDialog();
+  }
 
 }
